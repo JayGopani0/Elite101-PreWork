2f2ea7dfea702749cc7f33734b13086815b19750
JayGopani0 <jaygopani0@gmail.com>
2025-01-17 07:02:04 +0000

    update

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("Welcome to the food ordering Chatbot")
-name = input("What is your name? ")
-print("hello")
-print("hi")
+print("Hello! Welcome to the solar system Chatbot!")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f'Hello {name}, you have witnessed {age} rotations of the Earth around the sun!')
+print("How can I be of assitance?")
+print("")
+while True:
+    print("Choose from these options: ")
+    print("1. Placeholder option 1")
+    print("2. Placeholder option 2")
+    print("3. Placeholder option 3")
+    print("4. Exit the Chatbot")
+    choice = int(input("Enter a number to move through the different choices: "))
+    if choice == 4:
+        print("Thank you for speaking with Chatbot!")
+        break
+
